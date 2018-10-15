9bd8d5976c45279d13b05a8724af45ab1c5a376c
MahmoudAymen <35262171+MahmoudAymen@users.noreply.github.com>
2018-10-15 21:46:27 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Fichier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(444, 250);
+            this.textBox1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(361, 560);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(206, 75);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Open";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(396, 438);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(0, 16);
+            this.labelControl1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 660);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fichier
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+          
+        
+            OpenFileDialog open = new OpenFileDialog();
+            char separator = ' ';
+            char s1 = '.';
+            char[] s2 = { separator, s1 };
+            string[] tab = null;
+            int nboccurrence = 1;
+            open.Filter = "Fichiers texte|*.txt" ;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                string path = Path.GetExtension(open.FileName);
+                if (path == ".txt")
+                {
+                    textBox1.Text = File.ReadAllText(open.FileName);
+                     tab = File.ReadAllText(open.FileName).Split(s2);
+                 
+                }
+                else
+                {
+                    MessageBox.Show("n est pas de extension .txt");
+                }
+             
+                for(int i=0;i<tab.Length;i++)
+                {
+                    labelControl1.Text = tab[i];
+
+                    for (int j = i+1; j < tab.Length; j++)
+                    {
+                        if (tab[j].Equals(labelControl1.Text))
+                        {
+                        nboccurrence++;
+
+                            tab[j] = "";
+
+
+                        }
+                      
+
+
+
+                    }
+                    if (!tab[i].Equals(""))
+
+                    DevExpress.XtraEditors.XtraMessageBox.Show(nboccurrence.ToString());
+
+
+                    nboccurrence = 1;
+                    
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
